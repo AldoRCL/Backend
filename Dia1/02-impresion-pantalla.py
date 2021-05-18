51ccbc3a6372cc58815f5139c62f4db04571900f
AldoRCL <aldo_06_05@hotmail.com>
2021-05-17 20:08:39 -0500

    INICIO SEMANA1 DEFINICION, IMPRESION, OPERADORES

---
+++ Dia1/02-impresion-pantalla.py
@@ -0,0 +1,9 @@
+estacion = "otoño"
+# concatenar varios valores, siempre y cuando sean todos STRING y pondra un espacio entre valor y valor
+print("la estacion es:", estacion,".")
+alumnos = 22
+grupo = 6
+print("Los alumnos son: {} y son del grupo {} y todos don divertidos".format(alumnos, grupo))
+print("los alumnos del grupo {1} son en total {0}".format(alumnos, grupo))
+# la f fucniona como el format usado anteriormente
+print(f"los alumnos son en total {alumnos}")
---
+++ Dia1/04-operadores-asignacion.py
@@ -0,0 +1,14 @@
+ # OPERADORES DE ASIGNACION
+ # = IGUAL
+ x = 10
+ # += INCREMENTO
+ x += 1
+ # -= DECREMENTO
+ x -= 1
+ # *= MLTIPLICADOR
+ x *= 2
+ # /= DIVIDENDO
+ x /= 2
+ # ** POTENCIA
+ x **= 2
+ print(x)
