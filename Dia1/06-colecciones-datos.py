47bdda06a6a95a9f5eb2c2573b41d4e5d5212a43
AldoRCL <aldo_06_05@hotmail.com>
2021-05-18 22:05:33 -0500

    Dia 2

--- Dia1/06-colecciones-datos.py
+++ Dia1/06-colecciones-datos.py
@@ -29,3 +29,89 @@ print(colores)
 colore_eliminado = colores.pop(0)
 print(colores)
 print(colore_eliminado)
+
+# metodo para resetear toda la lista y dejarla en blanco
+colores.clear()
+print(colores)
+
+# TUPLAS => coleccion de elementos ordenada NO SE PUEDE MODIFICAR LUEGO DE SU CREACION
+notas = [14, 16, 17, 11, 5, 1, 5, 5, 5]
+print(notas[0])
+print(notas[-1])
+
+print(len(notas))
+#cantidad de elementos de la tupla notas es 6 elemntos
+print("la cantidad de elementos de la tupla notas es {} elementos".format(len(notas)))
+print(f"la cantidad de elementos de la tupla notas es {len(notas)} elementos")
+
+#ver si hay elemntos repetidos en una tupla
+print (notas.count(5))
+
+#CONJUNTOS => coleccion de elemntos DESORDENADA, osea que una vez creemos no podemos acceder a sus posiciones ya que se ordenaran aleatoreamente
+estaciones = {"VERANOS", "OTOÑO", "PRIMAVERA", "INVIERNO"}
+print(estaciones)
+estaciones.add("OTOÑOVERANO")
+# el metodo in sirve para validar si un valor esta dentro de una coleccion de datos 
+print("OTOÑOVERNAO" in estaciones)
+
+#DICCIONARIOS => coleccion de elemntos que estan INDEXADOS  , que nosotros manejamos el nombre de su llave
+persona = {
+    'id' : 1,
+    'nombre': 'Aldito',
+    "relacion": "Soltero",
+    "fecha_nacimiento": "1992/08/04",
+    "hobbies": [
+        {
+            "nombre":"futbol",
+            "conocimiento": "Intermedio"
+        },
+        {
+            "nombre": "Drones",
+            "conocimiento": "Basico"
+        }
+    ]
+}
+print(persona['nombre'])
+print(persona["hobbies"][0]['conocimiento'])
+persona['apellido'] = "Martinez"
+
+#para eliminar 
+
+
+libro = {
+    "nombre": "Harry Potter",
+    "autor": "J.K. Rowling",
+    "editorial": "Blablabla",
+    "año": 2018,
+    "idiomas": [
+        {
+            "nombre": "portuges"
+        },
+        {
+            "nombre": "ingles",
+            "nombre": "ingles britanico"
+        },
+        {
+            "nombre": "frances"
+        },
+        {
+            "nombre": "aleman"
+        },
+    ],
+    "calificacion": 5,
+    "imdb": "00asd12-asd878-a4s5d4a5-a45sd4a5sd",
+    "tomos": ("La piedra filosofal", "La camara secreta", "El vuelo del fenix")
+}
+
+# EJERCICIO 1
+# devolver el autor del libro
+print(libro["autor"])
+# EJERCICIO 2
+# devolver el segundo tomo
+print(libro["tomos"][1])
+# EJERCICIO 3
+# devolver la cantidad de idiomas del libro
+print(len(libro["idiomas"]))
+# EJERCICIO 4
+# indicar si esta o no esta el idioma ruso
+print(libro["idiomas"]["nombres"])
