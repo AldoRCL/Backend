964fc2b249403d5ea8f3941676c719434f8d2fcf
AldoRCL <aldo_06_05@hotmail.com>
2021-05-26 20:22:07 -0500

    IniciosBD

---
+++ Dia3/Inicios DB.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE pruebas;
+USE pruebas;
+
+CREATE TABLE alumnos(
+nombre varchar(40),
+apellido varchar(25),
+sexo varchar(10),
+numero_emergencia int,
+religion varchar(10),
+fecha_nacimiento date
+);
