eebddf9e27728af0c374d70e2dcae3fe22e2f5dc
AldoRCL <aldo_06_05@hotmail.com>
2021-06-01 18:50:27 -0500

    Hola

---
+++ Dia1/models/postres.py
@@ -0,0 +1,7 @@
+from Config.conexion_bd import base_de_datos
+from sqlalchemy import Column, types
+
+
+class PostreModel(base_de_datos.Model):
+    postreId = Column(name='id', primary_key=True,
+                      autoincrement=True, unique=True, type_=types.Integer)
