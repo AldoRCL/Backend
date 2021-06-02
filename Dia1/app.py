ac14704e5e3951161883138d9596e815e4b3ba3d
AldoRCL <aldo_06_05@hotmail.com>
2021-06-02 17:42:09 -0500

    Clase 3

--- Dia1/app.py
+++ Dia1/app.py
@@ -2,21 +2,37 @@ from flask import Flask, request
 # Para usar las variables declaradas en el archivo .env
 from dotenv import load_dotenv
 from os import environ
+
+from flask_sqlalchemy import model
+from Config.conexion_bd import base_de_datos
+from flask_restful import Api
+from controllers.postre import PostresController
+from models.postres import PostreModel
+from models.preparacion import PreparacionModel
+from models.ingredientes import IngredienteModel
+from models.receta import RecetaModel
+
 load_dotenv()
 
 app = Flask(__name__)
+api = Api(app)
 # https://docs.sqlalchemy.org/en/14/core/engines.html
 # https://flask-sqlalchemy.palletsprojects.com/en/2.x/config/#connection-uri-format
 # dialect://username:password@host:port/database
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("DATABASE_URI")
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+base_de_datos.init_app(app)
 
 
+base_de_datos.create_all(app=app)
 @app.route("/")
 def initial_controller():
     return {
         "message": "Bienvenido a mi API de RECETAS DE POSTRES 🎂"
     }
 
+api.add_resource(PostresController, "/postres")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Dia1/models/postres.py
+++ Dia1/models/postres.py
@@ -1,7 +1,25 @@
 from Config.conexion_bd import base_de_datos
-from sqlalchemy import Column, types
+from sqlalchemy import Column, types,orm
 
 
 class PostreModel(base_de_datos.Model):
+    #para cambia el nombre de la tabla en la base de datos
+    __tablename__ = "postres"
+
     postreId = Column(name='id', primary_key=True,
                       autoincrement=True, unique=True, type_=types.Integer)
+
+    postreNombre = Column(name='nombre',type_=types.String(length=45))
+    postrePorcion = Column(name='porcion', type_=types.String(length=25))
+    #rlationship isrve para indicar todos los "hijos" que puede tener ese modelo (todas sus FK)
+    ##el backref crearea un atributo virtual en el model del hijo (preparacio ) para que pueda acceder a todo el objeto de PotreModel sin la necesidad de hacer una sub consulta (creara un join cuando sea necesario )
+    #lazy => define cuando SQLAlchemy va a cargar la data adyacente de la base de datos
+    #Truer / 'select' => cargara osod los datos adyacentes
+    #False / 'join' => solamente cargara cuando sea necesario (cuando se utilicen dichos datos)
+    #'subquery' => trbajara los datos PERO en una sub consulta
+    #'dynamic' => en este se pueden agregar filtros adicionales.SQLAlchemy devolvera otro objeto dentro de la clase
+    preparaciones = orm.relationship('PreparacionModel',backref='postrePreparaciones',lazy=True)
+
+    def __init__(self, nombre, porcion):
+        self.postreNombre= nombre
+        self.postrePorcion= porcion
