500eb501e5e4f94987fe8966b825f4a1be7652d7
AldoRCL <aldo_06_05@hotmail.com>
2021-06-15 22:38:03 -0500

    Dia martes

---
+++ blog_personal/gestion/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import LibroModel, UsuarioModel
+
+
+class LibroAdmin(admin.ModelAdmin):
+    #modifica la vista del modelo
+    list_display = ['libroNombre','libroAutor','libroCantidad']
+    #agrega un buscador al modelo, y ademas hay que indicar a que columnas se realizara la busqueda cuando se tenga una 
+    search_fields =['libroNombre','libroEdicion']
+    #Agrega campo de busqueda rapida para listar una busqueda mas generica, se recomienda utilizar campos en los cuales no contengan muchos valores
+    list_filter =['libroAutor'] 
+    #indica si se desea ver algun campo que el ususario no puede manipular (solo lectura)
+    #los campos de solo lectura, se coloran al final , despues de los campos de escritura
+    readonly_fields = ['libroId']
+
+admin.site.register(LibroModel,LibroAdmin)
+admin.site.register(UsuarioModel)
